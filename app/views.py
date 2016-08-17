1f758eedc059a4587dfacf67baa993a3245b8cf3
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-08-17 18:50:54 -0400

    Adding home page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello World!!!"
+    user = { 'nickname': 'Ani' }
+    posts = [
+            { 'author': { 'nickname': 'John' }, 'body': 'Beautiful day in Miami!' },
+            { 'author': { 'nickname': 'Susan' }, 'body': 'The Avengers movie was cool.'}
+            ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
