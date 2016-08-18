db823b096e3b75f0ab49c31e321d54df3c6ec332
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-08-18 19:15:02 -0400

    Adding form validations

--- app/views.py
+++ app/views.py
@@ -13,8 +13,11 @@ def index():
             ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %(form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html', title='Sign in', form=form)
