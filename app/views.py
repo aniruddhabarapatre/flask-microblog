0a74b53cfb7906e87fdf9949b94905ca3f404384
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-08-18 18:38:12 -0400

    Creating login form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -11,3 +12,9 @@ def index():
             { 'author': { 'nickname': 'Susan' }, 'body': 'The Avengers movie was cool.'}
             ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign in', form=form)
